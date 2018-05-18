783f0d27d7a1f0613b794419f1b6e78693484c83
Khuong DUy <nguyenkhuongduy160996.inspi@gmail.com>
2018-05-18 23:19:03 +0700

    edit display of menu

--- src/screens/Menu.js
+++ src/screens/Menu.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { ToastAndroid, Text } from 'react-native'
+import { ToastAndroid, Text , Animated,StyleSheet, View,Easing} from 'react-native'
 import PropTypes from 'prop-types'
 import { Spinner } from 'native-base'
 import { connect } from 'react-redux'
@@ -20,10 +20,13 @@ import {
 
 const screenStyles = {
   padding: 30,
+  flex: 3 ,
 }
 
 const buttonStyles = {
   margin: 10,
+  borderRadius: 6,
+  backgroundColor:'#D2691E'
 }
 
 const spaceStyles = {
@@ -50,6 +53,16 @@ const fixedBottomStyles = {
   },
 )
 export default class Menu extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      move1: new Animated.Value(-700),
+      move2: new Animated.Value(-700),
+      color1: new Animated.Value(0),
+      trans: new Animated.Value(0),
+    }
+  }
+
   static navigationOptions = ({ screenProps }) => ({
     title: I18n.t('menu.header', screenProps.language),
   });
@@ -76,45 +89,164 @@ export default class Menu extends Component {
     .catch(err => ToastAndroid.show(err.message, ToastAndroid.LONG))
   };
 
+  componentDidMount() {
+    const ani1 = Animated.timing(
+        this.state.move1,
+        {
+            toValue: 0,
+            duration: 2000,
+            easing: Easing.bounce
+        }
+    );
+    const ani2 = Animated.timing(
+        this.state.move2,
+        {
+            toValue: 0,
+            duration: 2000,
+            easing: Easing.bounce
+        }
+    );
+    const aniColor1 = Animated.timing(
+        this.state.color1,
+        {
+            toValue: 1,
+            duration: 2000,
+        }
+    );
+    const aniColor2 = Animated.timing(
+        this.state.color1,
+        {
+            toValue: 0,
+            duration: 2000,
+        }
+    );
+    const trans1 = Animated.timing(
+        this.state.trans,
+        {
+            toValue: 1,
+            duration: 2000,
+        }
+    );
+    const trans2 = Animated.timing(
+        this.state.trans,
+        {
+            toValue: 0,
+            duration: 2000,
+        }
+    );
+    Animated.stagger(200, [ani1, ani2]).start();
+    Animated.loop(Animated.sequence([aniColor1, aniColor2])).start();
+    Animated.loop(Animated.sequence([trans1, trans2])).start();
+  }
   render() {
+    const marginLeft1 = this.state.move1;
+    const marginLeft2 = this.state.move2;
+    const backgroundColor = this.state.color1.interpolate({
+        inputRange: [0, 1],
+        outputRange: ['blue', 'yellow']
+    });
+    const trans = this.state.trans.interpolate({
+        inputRange: [0, 0.5, 1],
+        outputRange: ['15deg', '0deg', '-15deg']
+    })
     const { isLoading, isUpdating, updateProgress } = this.props
     const disableButtons = isLoading || isUpdating
     return (
-      <BackgroundView style={screenStyles}>
-        <Text style={spaceStyles}>
-            This is Quiz Appplications which was built by five dogs team. With varied of questions about life subject, fun subject...
-        </Text>
+      
+          <BackgroundView style={screenStyles}>
+            <View style={{flex:2}} >
+              <Text style={spaceStyles}>
+                  This is Quiz Appplications which was built by five dogs team. With varied of questions about life subject, fun subject...
+              </Text>
 
-        <Text style={spaceStyles}>
-          Now, you can start game to explore and feeling by way click on Play Game button below.
-        </Text>
+              <Text style={spaceStyles}>
+                Now, you can start game to explore and feeling by way click on Play Game button below.
+              </Text>
 
-        <Button
-          style={buttonStyles}
-          disabled={disableButtons}
-          title={I18n.t('btn.playGame')}
-          onPress={this.onGamePlayPressed}
-        />
-        <Button
-          style={buttonStyles}
-          disabled={disableButtons}
-          title={I18n.t('btn.checkUpdates')}
-          onPress={this.onUpdatePressed}
-        />
-        <Button
-          style={buttonStyles}
-          disabled={disableButtons}
-          title={I18n.t('btn.settings')}
-          onPress={this.onSettingsPressed}
-        />
+              <Button
+                style={buttonStyles}
+                disabled={disableButtons}
+                title={I18n.t('btn.playGame')}
+                onPress={this.onGamePlayPressed}
+              />
+              <Button
+                style={buttonStyles}
+                disabled={disableButtons}
+                title={I18n.t('btn.checkUpdates')}
+                onPress={this.onUpdatePressed}
+              />
+              <Button
+                style={buttonStyles}
+                disabled={disableButtons}
+                title={I18n.t('btn.settings')}
+                onPress={this.onSettingsPressed}
+              />
 
-        <Text style={fixedBottomStyles}>
-          Five Dogs Team.
-        </Text>
+              <Text style={fixedBottomStyles}>
+                Five Dogs Team.
+              </Text>
 
-        {disableButtons && <Spinner />}
-        {isUpdating && <ProgressBar progress={updateProgress} />}
-      </BackgroundView>
+              {disableButtons && <Spinner />}
+              {isUpdating && <ProgressBar progress={updateProgress} />}
+            </View>
+            <View style={sty.container}>
+              <Animated.View style={{
+                width: 80 ,
+                height: 70,
+                backgroundColor,
+                        //opacity
+                marginLeft: marginLeft1,
+                marginBottom: 20,
+                transform: [
+                  { rotate: trans },
+                ],
+
+                        // ...this.props.style
+              }} >
+                <Text style={sty.text}>good</Text>
+              </Animated.View>
+
+              <Animated.View style={{
+                width: 80,
+                height: 70,
+                backgroundColor,
+                      
+                marginLeft: marginLeft2,
+                transform: [
+                  { rotate: trans },
+                ],
+
+                        
+              }} >
+
+                <Text style={sty.text}>luck</Text>
+
+
+              </Animated.View>
+          </View>
+        </BackgroundView>
+       
     )
   }
 }
+
+const sty = StyleSheet.create({
+  container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'lightyellow'
+  },
+  ani: {
+      width: 100,
+      height: 100,
+      backgroundColor: 'blue',
+      //opacity: opacity
+  },
+  text: {
+      fontSize: 25,
+      color: 'red',
+      marginTop: 20,
+      marginLeft: 18,
+  },
+})
